07ed8ecf25ce2e39a2b2bbe56fbb2503900fcf7b
dilaouid <yedine@live.fr>
2018-12-27 20:38:14 +0100

    complete checks functions

--- checks.c
+++ checks.c
@@ -6,11 +6,24 @@
 /*   By: dilaouid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/21 17:57:39 by dilaouid          #+#    #+#             */
-/*   Updated: 2018/12/24 19:01:10 by dilaouid         ###   ########.fr       */
+/*   Updated: 2018/12/23 23:18:20 by dilaouid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
+
+int	check_links(char **board, int *link, int x, int y)
+{
+	if (y > 0 && board[y - 1][x] == '#')
+		*link += 1;
+	if (board[y + 1][x] && board[y + 1][x] == '#')
+		*link += 1;
+	if (board[y][x + 1] && board[y][x + 1] == '#')
+		*link += 1;
+	if (x > 0 && board[y][x - 1] == '#')
+		*link += 1;
+	return (*link);
+}
 
 int	check_rows(char **line, int nbline)
 {
@@ -25,10 +38,8 @@ int	check_rows(char **line, int nbline)
 		{
 			rows++;
 			y++;
-			if (y == nbline && rows == 4)
+			if (y == nbline)
 				return (1);
-			else if (y == nbline && rows != 4)
-				return (0);
 		}
 		if (rows != 4)
 			return (0);
@@ -80,7 +91,7 @@ int	check_emptyboard(char **line)
 		}
 		if (line[y][0] == '\n')
 			dot = 0;
-		if (dot == 16)
+		if (y % 4 == 0 && y > 0 && dot == 16)
 			return (0);
 		y++;
 	}
@@ -92,31 +103,31 @@ int	check_validtetriminos(char **line)
 	int x;
 	int y;
 	int shapesize;
+	int link;
 
 	ft_putendl("\n-------CHECK IF THE TETRIMINOS ARE VALID ...-------");
 	y = 0;
+	link = 0;
 	shapesize = 0;
 	while (line[y])
 	{
 		x = 0;
 		while (line[y][x])
 		{
-			ft_putchar(line[y][x]);
-			if (x == 0 && y == 0 && line[y][x] == '#'
-					&& (line[y][x + 1] != '#' || line[y + 1][x] != '#'))
-				return (0);
-			else if (line[y][x] == '#' && (((x > 0 && line[y][x - 1] != '#')
-						&& (line[y][x + 1] != '\n' && line[y][x + 1] != '#'))
-						&& ((y > 0 && line[y - 1][x] != '#') && (line[y + 1]
-						&& line[y + 1][x] != '#'))) && line[y][x + 1] != '\n')
-				return (0);
 			if (line[y][x] == '#')
+			{
 				shapesize++;
+				check_links(line, &link, x, y);
+			}
 			x++;
 		}
+		if (line[y][0] == '\n' && link != 6 && link != 8)
+			return (0);
+		else if (line[y][0] == '\n')
+			link = 0;
 		y++;
 	}
-	if (shapesize % 4 != 0 || shapesize >= y)
+	if (shapesize % 4 != 0)
 		return (0);
 	ft_putendl(GREEN"VALID TETRIMINOS"RESET);
 	return (1);
